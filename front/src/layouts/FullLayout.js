a8510b3cc6fd7153003f1f2aebbc84e1a75917ba
onlyhappiness <onlyhappiness96@gmail.com>
2022-06-04 21:36:39 +0900

    [ Delete ] Unused File

--- front/src/layouts/FullLayout.js
+++ front/src/layouts/FullLayout.js
@@ -1,45 +1,44 @@
-import React from "react";
+import React from 'react';
 import {
   experimentalStyled,
   useMediaQuery,
   Container,
   Box,
-} from "@mui/material";
-import Header from "./header/Header";
-import Sidebar from "./sidebar/Sidebar";
-import Footer from "./footer/Footer";
+} from '@mui/material';
+import Header from './header/Header';
+import Sidebar from './sidebar/Sidebar';
 
-const MainWrapper = experimentalStyled("div")(() => ({
-  display: "flex",
-  minHeight: "100vh",
-  overflow: "hidden",
-  width: "100%",
+const MainWrapper = experimentalStyled('div')(() => ({
+  display: 'flex',
+  minHeight: '100vh',
+  overflow: 'hidden',
+  width: '100%',
 }));
 
-const PageWrapper = experimentalStyled("div")(({ theme }) => ({
-  display: "flex",
-  flex: "1 1 auto",
-  overflow: "hidden",
+const PageWrapper = experimentalStyled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  overflow: 'hidden',
 
   backgroundColor: theme.palette.background.default,
-  [theme.breakpoints.up("lg")]: {
-    paddingTop: "64px",
+  [theme.breakpoints.up('lg')]: {
+    paddingTop: '64px',
   },
-  [theme.breakpoints.down("lg")]: {
-    paddingTop: "64px",
+  [theme.breakpoints.down('lg')]: {
+    paddingTop: '64px',
   },
 }));
 
 const FullLayout = ({ children }) => {
   const [isSidebarOpen, setSidebarOpen] = React.useState(true);
   const [isMobileSidebarOpen, setMobileSidebarOpen] = React.useState(false);
-  const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"));
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
   return (
     <MainWrapper>
       <Header
         sx={{
-          paddingLeft: isSidebarOpen && lgUp ? "265px" : "",
-          backgroundColor: "#fbfbfb",
+          paddingLeft: isSidebarOpen && lgUp ? '265px' : '',
+          backgroundColor: '#fbfbfb',
         }}
         toggleMobileSidebar={() => setMobileSidebarOpen(true)}
       />
@@ -52,12 +51,11 @@ const FullLayout = ({ children }) => {
         <Container
           maxWidth={false}
           sx={{
-            paddingTop: "20px",
-            paddingLeft: isSidebarOpen && lgUp ? "280px!important" : "",
+            paddingTop: '20px',
+            paddingLeft: isSidebarOpen && lgUp ? '280px!important' : '',
           }}
         >
-          <Box sx={{ minHeight: "calc(100vh - 170px)" }}>{children}</Box>
-          <Footer />
+          <Box sx={{ minHeight: 'calc(100vh - 170px)' }}>{children}</Box>
         </Container>
       </PageWrapper>
     </MainWrapper>
--- front/src/layouts/sidebar/MenuItems.js
+++ front/src/layouts/sidebar/MenuItems.js
@@ -1,43 +1,34 @@
 const Menuitems = [
   {
-    title: "Dashbaord",
-    icon: "home",
-    href: "/",
+    title: 'Dashbaord',
+    icon: 'home',
+    href: '/',
   },
   {
-    title: "Buttons",
-    icon: "disc",
-    href: "/buttons",
+    title: 'Buttons',
+    icon: 'disc',
+    href: '/buttons',
   },
   {
-    title: "Forms",
-    icon: "layout",
-    href: "/forms",
+    title: 'Forms',
+    icon: 'layout',
+    href: '/forms',
   },
   {
-    title: "Alerts",
-    icon: "info",
-    href: "/alerts",
+    title: 'Alerts',
+    icon: 'info',
+    href: '/alerts',
+  },
+
+  {
+    title: 'Pagination',
+    icon: 'user',
+    href: '/pagination',
   },
   {
-    title: "Ratings",
-    icon: "star",
-    href: "/rating",
-  },
-  {
-    title: "Images",
-    icon: "image",
-    href: "/image",
-  },
-  {
-    title: "Pagination",
-    icon: "user",
-    href: "/pagination",
-  },
-  {
-    title: "Tables",
-    icon: "grid",
-    href: "/table",
+    title: 'Tables',
+    icon: 'grid',
+    href: '/table',
   },
 ];
 
