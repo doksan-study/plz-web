7233c5d5d6795db0eb83b46eb74081ea558674fe
onlyhappiness <onlyhappiness96@gmail.com>
2022-06-07 18:16:42 +0900

    [ Progress ] ProductDetail Slider 구현 중..

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -8,6 +8,7 @@ import theme from "../src/theme/theme";
 import createEmotionCache from "../src/createEmotionCache";
 import FullLayout from "../src/layouts/FullLayout";
 import "../styles/style.css";
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
--- front/src/components/product/productDetailCard.jsx
+++ front/src/components/product/productDetailCard.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import Image from "next/image";
 import {
   Button,
   Card,
@@ -5,30 +7,66 @@ import {
   CardMedia,
   Grid,
   Typography,
-} from '@mui/material';
+} from "@mui/material";
+
+import Slider from "react-slick";
 
 const ProductDetailCard = () => {
+  const slides = [
+    {
+      id: 1,
+      image:
+        "https://i.pinimg.com/564x/81/60/03/8160038f95c8c4b09949246aac5a14a6.jpg",
+    },
+    {
+      id: 2,
+      image:
+        "https://i.pinimg.com/564x/d5/3b/ef/d53bef6aae2bad1eb1746a0edd70de14.jpg",
+    },
+    {
+      id: 3,
+      image:
+        "https://i.pinimg.com/564x/71/61/b6/7161b6140c5cf22284d9613750ca2650.jpg",
+    },
+  ];
+
+  // TODO: Slider settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <Card>
       <Grid container spacing={4}>
         <Grid item xs={12} lg={5}>
-          <CardMedia
-            component='img'
-            image='https://5.imimg.com/data5/HL/HS/ZC/SELLER-17552598/nike-airmax-tube-shoes-1000x1000.jpg'
-            alt='https://5.imimg.com/data5/HL/HS/ZC/SELLER-17552598/nike-airmax-tube-shoes-1000x1000.jpg'
-          />
+          <>
+            <div>테스트 slider</div>
+            <Slider {...settings}>
+              <div></div>
+            </Slider>
+          </>
+
+          {/* <CardMedia
+            component="img"
+            image="https://5.imimg.com/data5/HL/HS/ZC/SELLER-17552598/nike-airmax-tube-shoes-1000x1000.jpg"
+            alt="https://5.imimg.com/data5/HL/HS/ZC/SELLER-17552598/nike-airmax-tube-shoes-1000x1000.jpg"
+          /> */}
         </Grid>
         <Grid item xs={12} lg={7}>
           <CardContent>
-            <Typography variant='h3' gutterBottom>
+            <Typography variant="h3" gutterBottom>
               <strong>Nike</strong>
             </Typography>
-            <Typography variant='body1' color='text.secondary' gutterBottom>
+            <Typography variant="body1" color="text.secondary" gutterBottom>
               Nike Dunk Low SE Seoul
             </Typography>
           </CardContent>
           <CardContent>
-            <Typography variant='h3' gutterBottom>
+            <Typography variant="h3" gutterBottom>
               <strong>$546,00</strong>
             </Typography>
           </CardContent>
@@ -36,12 +74,12 @@ const ProductDetailCard = () => {
         <Grid item xs={6}>
           <Button
             sx={{
-              borderRadius: '12px',
+              borderRadius: "12px",
               p: 2,
-              width: '100%',
+              width: "100%",
             }}
-            variant='contained'
-            color='secondary'
+            variant="contained"
+            color="secondary"
           >
             BUY NOW
           </Button>
@@ -49,12 +87,12 @@ const ProductDetailCard = () => {
         <Grid item xs={6}>
           <Button
             sx={{
-              borderRadius: '12px',
+              borderRadius: "12px",
               p: 2,
-              width: '100%',
+              width: "100%",
             }}
-            variant='contained'
-            color='primary'
+            variant="contained"
+            color="primary"
           >
             Add to Cart
           </Button>
