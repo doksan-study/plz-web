45b3917742488ed225bfa416b23b08342ab5faab
onlyhappiness <onlyhappiness96@gmail.com>
2022-06-12 00:04:21 +0900

    [ Setup ] React-Query Get Test

---
+++ front/api/product.js
@@ -0,0 +1,39 @@
+import { product } from './index';
+
+// 상품 목록
+function productListAPI() {
+  console.log('요청하는 api========', `/list?page=1&limit=10`);
+  return product.get(`/list?page=1&limit=10`);
+}
+
+// 상품 상세 정보
+function productDetailAPI(id) {
+  console.log('요청하는 api=======', `/product/${id}`);
+  return product.get(`/list/${id}`);
+}
+
+// 상품 등록
+function productCreateAPI(data) {
+  console.log('요청하는 api=======', `/product`);
+  return product.post(``, data);
+}
+
+// 상품 수정
+function productModifyAPI(data) {
+  console.log('요청하는 api=======', `/product`);
+  return product.put(``, data);
+}
+
+// 상품 삭제
+function productDeleteAPI(id) {
+  console.log('요청하는 api=======', `/product/${id}`);
+  return product.get(`/list/${id}`);
+}
+
+export {
+  productListAPI, // 상품 목록
+  productDetailAPI, // 상품 상세 정보
+  productCreateAPI, // 상품 등록
+  productModifyAPI, // 상품 수정
+  productDeleteAPI, // 상품 삭제
+};
--- front/src/components/dashboard/ProductOverview.jsx
+++ front/src/components/dashboard/ProductOverview.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Card,
   CardContent,
@@ -5,14 +6,17 @@ import {
   CardMedia,
   Grid,
   Typography,
-} from "@mui/material";
-import React from "react";
-import { useRouter } from "next/router";
+} from '@mui/material';
+import { useRouter } from 'next/router';
 
-import { shoes } from "../../../data/shoesData";
+import { shoes } from '../../../data/shoesData';
+import { productListData } from '../../../hooks/product';
 
 const ProductOverview = () => {
   const router = useRouter();
+
+  const { isLoading, data: productList } = productListData();
+  console.log('불러오는 데이터 목록======================', data);
 
   return (
     <Grid container spacing={2}>
@@ -21,12 +25,12 @@ const ProductOverview = () => {
           <Grid item xs={6} md={4} key={data.id}>
             <Card
               sx={{
-                cursor: "pointer",
-                height: "450px",
+                cursor: 'pointer',
+                height: '450px',
               }}
               onClick={() => {
                 router.push({
-                  pathname: "/product/[id]",
+                  pathname: '/product/[id]',
                   query: {
                     id: data.id,
                   },
@@ -35,22 +39,22 @@ const ProductOverview = () => {
             >
               <CardHeader title={data.name} subheader={data.description} />
               <CardMedia
-                component="img"
-                height="194"
+                component='img'
+                height='194'
                 image={data.thumbnail}
                 alt={data.thumbnail}
               />
               <CardContent
                 sx={{
-                  textAlign: "center",
+                  textAlign: 'center',
                   mt: 2,
                 }}
               >
-                <Typography variant="h4" mb={1}>
+                <Typography variant='h4' mb={1}>
                   {data.description}
                 </Typography>
-                <Typography variant="body1" color="text.secondary">
-                  {data.cost?.toLocaleString("ko-KR")}원
+                <Typography variant='body1' color='text.secondary'>
+                  {data.cost?.toLocaleString('ko-KR')}원
                 </Typography>
               </CardContent>
             </Card>
